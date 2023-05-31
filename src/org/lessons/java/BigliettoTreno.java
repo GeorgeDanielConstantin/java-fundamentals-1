3397ea22d5834cf667c19aaf94c2a0166bbb8cb2
GeorgeDanielConstantin <danielgconstantin@gmail.com>
2023-05-31 14:34:54 +0200

    Add biglietto treno

---
+++ src/org/lessons/java/BigliettoTreno.java
@@ -0,0 +1,35 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+
+public class BigliettoTreno {
+    public static void main(String[] args) {
+        Scanner keyboardReader = new Scanner(System.in);
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+
+        int km;
+        int age;
+        double pricePerKm = 0.21;
+        double finalPrice;
+        double underageDiscount = 0.8;
+        double overageDiscount = 0.6;
+
+
+        System.out.print("Km da percorrere: ");
+        km = keyboardReader.nextInt();
+
+        System.out.print("Età ");
+        age = keyboardReader.nextInt();
+
+        if (age<18) {
+            finalPrice = (km * pricePerKm) * underageDiscount;
+        } else if (age>64) {
+            finalPrice = (km * pricePerKm) * overageDiscount;
+        } else {
+            finalPrice = km * pricePerKm;
+        }
+        System.out.print("Il prezzo è :" + decimalFormat.format(finalPrice));
+    }
+}
